502e40b30485d805740c4d4970bd609fb38a3fa6
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-13 16:31:21 +0200

    added js html css files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*
+1. TuttiFrutti
+Cartella array-lista
+Descrizione:
+Abbiamo un frigorifero pieno di frutta:
+'banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'  
+C'è anche una pesca sul tavolo, la mettiamo nel frigo.
+Stasera dobbiamo fare un cocktail a base di cocomero: ce l'abbiamo o no in frigo?
+Fasi
+1. creare l'array con la frutta del frigorifero
+2. aggiungere la pesca all'array della frutta
+3. verificare se nell'array di frutta c'è il cocomero:
+   - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
+   - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+Suggerimenti/Indicazioni:
+Sì, lo sappiamo che esiste la funzione includes() ma non non la vogliamo utilizzare per cercare nel frigorifero.
+*/
+
+//1. creare l'array con la frutta del frigorifero
+const fridge = [
+    'banana',
+    'mela',
+    'pera', 
+    'ciliegia',
+    'arancia',
+    'mandarino',
+    'cocomero',
+    'limone',
+    'fragola'
+]
+//2. aggiungere la pesca all'array della frutta
+fridge.push('pesca');
+/*
+3. verificare se nell'array di frutta c'è il cocomero:
+- se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
+- se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
+*/
+
+
