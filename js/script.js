d219a0f4f9958a2d38042790020b51ab9f3a21eb
Finnedorn <daniele.giuntoli@outlook.com>
2023-10-13 19:41:50 +0200

    js: added last 5 extracted numbers

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ Suggerimenti/Indicazioni:
 Sì, lo sappiamo che esiste la funzione includes() ma non non la vogliamo utilizzare per cercare nel frigorifero.
 */
 
+
+/*
 //1. creare l'array con la frutta del frigorifero
 const fridge = [
     'banana',
@@ -30,25 +32,88 @@ const fridge = [
 ];
 //2. aggiungere la pesca all'array della frutta
 fridge.push('pesca');
+*/
 /*
 3. verificare se nell'array di frutta c'è il cocomero:
 - se c'è stampiamo: "Trovato! Devo solo preparare il cocktail."
 - se non lo trovo: "Oh no, devo uscire a comprare il cocomero!"
 */
+/*
 let watermelon = false;
-
 for (i = 0; i <= fridge.length; i++) {
     if ( fridge[i] == 'cocomero') {
         watermelon = true
     }
 };
-
 if (watermelon) {
     console.log('Trovato! Devo solo preparare il cocktail.')
 } else {
     console.log('Oh no, devo uscire a comprare il cocomero!')
 };
+*/
 
+
+
+/*
+2. La coda dell'Array
+Cartella: array_tail
+Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e 
+generando tanti numeri casuali da 1 a 100 per quanti sono gli elementi da inserire.
+Stampiamo poi gli ultimi 5 elementi dell'Array
+Bonus:
+Chiediamo all'utente quanti elementi dell'array dovremo stampare
+Suggerimenti:
+Probabilmente conviene controllare che il numero di elementi della coda
+non sia più grande del numero di elementi totali dell'array
+*/
+
+const text = document.getElementById('data');
+
+const btn = document.querySelector('.btn');
+
+console.log(text, btn);
+
+const result = document.getElementById('result');
+
+
+
+btn.addEventListener('click', function() {
+    let arrnumber = parseInt(text.value);
+    //creo una let array in cui inserirò i valori generati dal for
+    let list = [];
+    console.log(arrnumber, list);
+    //creo un ciclo che cicli tanto quanto il valore inserito nell'input
+    //in questo modo però il sistema mi tirerà fuori un solo valore cioè l'ultimo generato
+    for(i = 0; i < arrnumber; i++) {
+        //pertento voglio che per ogni valore generato pushi un numero a caso estratto dal rng in un array dichiarato prima 
+        list.push(getRndInteger(1, 101));
+    };
+    //creo una let che mi estragga gli ultimi 5 elementi della array creata
+    //in questo diciamo: lastfive è uguale ad un array composto dagli elementi -5 di list, -4 di list, -3 di list...
+    let lastfive = [
+        (list[list.length - 5]),
+        (list[list.length - 4]),
+        (list[list.length - 3]),
+        (list[list.length - 2]),
+        (list[list.length - 1]),
+    ];
+    //adesso stampameli in console
+    console.log(lastfive);
+
+    if (arrnumber <= 5 ) {
+        result.innerHTML =`Ecco gli ultimi valori: ${list}.` 
+    } else {
+        result.innerHTML =`Ecco gli ultimi 5 valori: ${lastfive}.`
+    }
+});
+
+
+
+
+//contatore randomico
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 
